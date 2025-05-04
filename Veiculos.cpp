1e9dbd36416edb3995b2f1a404999a26f49a3998
Lana Ramos <lanadefy@gmail.com>
2025-05-04 16:49:18 -0300

    Correção de erros

--- Veiculos.cpp
+++ Veiculos.cpp
@@ -4,32 +4,24 @@
 
 using namespace std;
 
-class Veiculo
-{
-    private:
-        string placa;
-        int tipo_do_veiculo;
-    public:
-        Veiculo()
-        {
-            placa = "Não informado";
-            tipo_do_veiculo = 0;
-        }
-        ~Veiculo(){}
-        void set_dados()
-        {
-            cout << "\nDigite a placa do veículo: ";
-            getline(cin>>ws,placa);
-            cout << "\nDigite se o veículo é \"comum (1)\" ou \"prioritário (2)\": ";
-            cin >> tipo_do_veiculo;
-        }
-        void print_dados()
-        {
-            cout << "\nPlaca: " << placa << "\tPrioridade: " << tipo_do_veiculo;
-        }
-        int ret_tipo()
-        {
-            return tipo_do_veiculo;
-        }
-        
-};
+Veiculo::Veiculo()
+    {
+        placa = "Não informado";
+        tipo_do_veiculo = 0;
+    }
+Veiculo::~Veiculo(){}
+void Veiculo::set_dados()
+    {
+        cout << "\nDigite a placa do veículo: ";
+        getline(cin>>ws,placa);
+        cout << "\nDigite se o veículo é \"comum (1)\" ou \"prioritário (2)\": ";
+        cin >> tipo_do_veiculo;
+    }
+void Veiculo::print_dados()
+    {
+        cout << "\nPlaca: " << placa << "\tPrioridade: " << tipo_do_veiculo;
+    }
+int Veiculo::ret_tipo()
+    {
+        return tipo_do_veiculo;
+    }
